b12b55fa7277393a50027bd362cbcfdb37bc6b6e
Elton <pm2014liberty@gmail.com>
2015-01-26 10:25:31 +0800

    build basic html layout

--- index.php
+++ index.php
@@ -1,2 +1,42 @@
-<?php
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>github g9007676/Instafeed</title>
+    <meta charset="utf-8">
+    <link href="assets/css/bootstrap.css" rel=stylesheet type="text/css">
+    <link href="assets/css/bootstrap-theme.min.css" rel=stylesheet type="text/css">
+    <script type="text/javascript" src="assets/js/instafeed.min.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <p class="navbar-brand" href="#">Github g9007676/Instafeed</p>
+        </div>
+    </div>
+</nav>
+<div class="jumbotron">
+    <div class="container">
+        <h1>Hello, world!</h1>
+        <p>I saw this js library would like to try to make a website
+        
+        Enjoy it instafeed.js</p>
+        <p><a class="btn btn-primary btn-lg" href="https://github.com/g9007676/Instafeed" role="button">My GitHub »</a></p>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-6 col-md-4">
+
+            <div class="thumbnail">
+                <img src="..." alt="...">
+                <div class="caption">
+                    <p>...</p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+</body>
+<html>
