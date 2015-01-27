d3d69f6a74bbf16818ab4b776e1b6fafc8997ed8
Elton <pm2014liberty@gmail.com>
2015-01-27 11:54:40 +0800

    add font

--- api.php
+++ api.php
@@ -1,15 +1,38 @@
 <?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+require('class/Transform.php');
+require('class/Curl.php');
 $client_id = 'd8354916c91f43e2badf7b318b115085';
 $client_secret = '50896f9726f9431293f3fc83d079890d';
 $website = 'http://10.1.1.95/';
 $redirect_url = 'http://10.1.1.95/Zheyu/Instafeed/api.php';
 
-// user id
-//
-//https://api.instagram.com/v1/users/search?q=g9007676&client_id=d8354916c91f43e2badf7b318b115085
+if ($_GET['username']) {
 
-//https://api.instagram.com/v1/users//media/recent/?q=g9007676&client_id=d8354916c91f43e2badf7b318b115085
-//
+    //search user id api url
+    $url = "https://api.instagram.com/v1/users/search?q={$_GET['username']}&client_id=d8354916c91f43e2badf7b318b115085";
+
+    $output = Curl::Request($url);
+
+    $user = json_decode($output, true);
+
+    if (200 != $user['meta']['code']) {
+        die('user not exist');
+    }
+
+    $userid = $user['data'][0]['id'];
+    $userinfo = $user['data'][0];
+    //get all user data, comment, photos..
+    $url = "https://api.instagram.com/v1/users/{$userid}/media/recent?client_id=d8354916c91f43e2badf7b318b115085";
+
+    $output = Curl::Request($url);
+
+    $transform = new Transform($output);
+
+    $photos = $transform->getPhoto();
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -18,6 +41,12 @@ $redirect_url = 'http://10.1.1.95/Zheyu/Instafeed/api.php';
     <meta charset="utf-8">
     <link href="assets/css/bootstrap.css" rel=stylesheet type="text/css">
     <link href="assets/css/bootstrap-theme.min.css" rel=stylesheet type="text/css">
+    <style>
+#bk_img{
+     background-image:url('http://subtlepatterns.com/patterns/skulls.png'); 
+     background-repeat: repeat; 
+}
+    </style>
 </head>
 <body>
 <nav class="navbar navbar-inverse navbar-fixed-top">
@@ -27,11 +56,11 @@ $redirect_url = 'http://10.1.1.95/Zheyu/Instafeed/api.php';
         </div>
     </div>
 </nav>
-<div class="jumbotron">
+<div style='color: inherit;background-color: #eee;'>
     <div class="container">
         <div class="row">
-            <div class="col-xs-6">
-                <h1>Use Instagram API</h1>
+            <div class="col-xs-6" style='margin-top: 100px'>
+                <h1 style='height:50px'>Use Instagram API</h1>
                 <p><a class="btn btn-primary btn-lg" href="https://github.com/g9007676/Instafeed" role="button">My GitHub »</a></p>
 
             </div>
@@ -46,11 +75,50 @@ $redirect_url = 'http://10.1.1.95/Zheyu/Instafeed/api.php';
                     </div>
                     <button type="submit" class="btn btn-primary">Transfer</button>
                 </form>
+
+                <?php if(! empty($userinfo)) { ?>
+                <div class="media" style='background:url(http://subtlepatterns.com/patterns/swirl_pattern.png)'>
+                    <div class="media-left">
+                        <a class='thumbnail'href="http://instagram.com/<?= $userinfo['username']?>">
+                            <img class="media-object" src="<?= $userinfo['profile_picture'] ?>" alt="...">
+                        </a>
+                    </div>
+                    <div class="media-body">
+                        <h4 class="media-heading"><?= $userinfo['full_name'] ?></h4>
+                        <h6 style='line-height: 2;'><?= nl2br($userinfo['bio'])?></h6>
+                    </div>
+                </div>
+                <? } ?>
             </div>
         </div>
     </div>
-<div class="container">
-<div id="instafeed"></div>
+</div>
+<div id='bk_img'>
+    <div class="container">
+        <div class="row">
+<?php if(! empty($photos)) {
+    foreach ($photos as $photo) { ?>
+            <div class="col-xs-4">
+                <a href='<?= $photo['link'] ?>'class="thumbnail">
+                    <img src="<?= $photo['img']['url']?>">
+                    <div class="caption">
+                        <? if (! empty($photo['local'])) { ?>
+                        <h3><span class="glyphicon glyphicon-globe" aria-hidden="true"></span>
+                            <?= $photo['local']?></h3>
+                        <? } ?>
+                        <h3><span class="glyphicon glyphicon-heart-empty" aria-hidden="true"></span>
+                            <?= $photo['like']?></h3>
+
+                        <?php if (!empty($photo['text'])) { ?>
+                        <span><?= nl2br($photo['text'])?> </span>
+                        <? }?>
+                    </div>
+                </a>
+            </div>
+            <? } ?>
+            <? } ?>
+        </div>
+    </div>
 </div>
 </body>
 <html>
--- class/Transform.php
+++ class/Transform.php
@@ -2,5 +2,66 @@
 
 class Transform
 {
+    public function __construct($json)
+    {
+        $data = json_decode($json, true);
+
+        if (200 != $data['meta']['code']) {
+            return false;
+        }
+
+        $this->data = $data;
+    }
+
+    public static function getUserInfo($user)
+    {
+        $user = json_decode($user, true);
+
+        if ($user['meta']['data']) {
+            die('user not exist');
+        }
+
+        return $user;
+    }
+
+    public function getPhoto()
+    {
+        if (empty($this->data['data'])) {
+            return false;
+        }
+
+        $data = $this->data['data'];
+        $arys= array();
+
+        foreach ($data as $val) {
+            $ret = array();
+
+            $ret['created_time'] = $val['created_time'];
+            $ret['local'] = $val['location']['name'];
+            $ret['text'] = $val['caption']['text'];
+            $ret['link'] = $val['link'];
+            $ret['like'] = $val['likes']['count']; 
+            $ret['img'] = $val['images']['low_resolution'];
+            $ret['img_ori'] = $val['images']['standard_resolution'];
+            $ret['img_thu'] = $val['images']['thumbnail'];
+
+            $arys[] = $ret;
+        }
+
+
+        return $arys;
+    }
+    
+    public function getNextUrl()
+    {
+        return $this->data['pagination']['next_url'];
+    }
+
+    
+    public function getNextMaxId()
+    {
+        return $this->data['pagination']['next_max_id'];
+    }
+
 
 }
